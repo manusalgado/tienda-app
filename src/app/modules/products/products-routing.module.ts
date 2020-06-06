f73c5f309018f067bdc5aa8658396ab2e3af3840
Manuel Salgado <manuel.salgado@rappi.com>
2020-06-05 20:54:44 -0500

    create form

--- src/app/modules/products/products-routing.module.ts
+++ src/app/modules/products/products-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductsContainer } from './products.container';
+import { ProductComponent } from './components/product/product.component';
 
 
 @NgModule({
@@ -10,6 +11,10 @@ import { ProductsContainer } from './products.container';
         path: '',
         component: ProductsContainer,
       },
+      {
+        path: 'create',
+        component: ProductComponent
+      }
     ]),
   ],
   exports: [RouterModule],
--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -7,18 +7,23 @@ import { ProductRootReducer } from './store/reducers';
 import { ProductsTableComponent } from './components/products-table/products-table.component';
 import { ProductsContainer } from './products.container';
 import { ProductsRoutingModule } from './products-routing.module';
+import { RouterModule } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 export const PRODUCT_REDUCER_TOKEN = new InjectionToken<
   ActionReducerMap<ProductState>
 >('Feature ProductModule Reducers');
 
 @NgModule({
-  declarations: [ProductsContainer, ProductsTableComponent],
+  declarations: [ProductsContainer, ProductsTableComponent, ProductComponent],
   imports: [
     CommonModule,
     StoreModule.forFeature('productsModule', PRODUCT_REDUCER_TOKEN),
     EffectsModule.forFeature([]),
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    RouterModule,
+    ReactiveFormsModule
   ],
   providers: [
     { provide: PRODUCT_REDUCER_TOKEN, useValue: ProductRootReducer },
